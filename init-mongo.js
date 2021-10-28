d87ed81e4640b4a83afc923db5fe3f4b174d88d2
Hamilkar247 <adonnibal96@gmail.com>
2021-10-28 17:58:21 +0200

    [WO-0] initial commit
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ init-mongo.js
@@ -0,0 +1,11 @@
+db.createUser(
+  {
+    user  : "YourUsername",
+    pwd   : "YourPasswordHere",
+    roles : [
+      {
+        role : "readWrite",
+        db   : "your-database-name"
+    ]
+  }
+)
